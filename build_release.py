0d5ddc32476d14d48477fedba1b0a127c22db1b4
Henrik <henrik.enquist@gmail.com>
2023-12-25 22:28:50 +0100

    Simplify, add poetry

--- build_release.py
+++ build_release.py
@@ -1,5 +1,7 @@
 import yaml
-from os.path import join
+import os
+import stat
+
 from jinja2 import Environment, FileSystemLoader
 
 with open("versions.yml") as f:
@@ -10,15 +12,24 @@ environment = Environment(loader=FileSystemLoader("templates/"))
 templates = [
     "requirements.txt",
     "cdsp_conda.yml",
-    "install_venv.sh",
-    "install_conda.sh",
-    "install_venv.bat",
-    "install_conda.bat",
+    "full_install_venv.sh",
+    "full_install_conda.sh",
+    "full_install_poetry.sh",
+    "full_install_venv.bat",
+    "full_install_conda.bat",
+    "pyproject.toml",
 ]
 
 for template in templates:
-    t = environment.get_template(template + ".jinja")
+    t = environment.get_template(template + ".j2")
 
+    # render and write
     rendered = t.render(versions)
-    with open(join("output", template), mode="w", encoding="utf-8") as f:
+    rendered_name = os.path.join("output", template)
+    with open(rendered_name, mode="w", encoding="utf-8") as f:
         f.write(rendered)
+
+    # make shell scripts executable
+    if rendered_name.endswith(".sh"):
+        file_stat = os.stat(rendered_name)
+        os.chmod(rendered_name, file_stat.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
